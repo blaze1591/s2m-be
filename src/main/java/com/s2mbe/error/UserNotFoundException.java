25b7f99d85b5dc76132c29aa955c9c7e9a441662
Alexander <alex.kozhuchar@gmail.com>
2017-12-03 14:42:15 +0200

    Localization for user not found

--- src/main/java/com/s2mbe/error/UserNotFoundException.java
+++ src/main/java/com/s2mbe/error/UserNotFoundException.java
@@ -3,6 +3,6 @@ package com.s2mbe.error;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "User not found")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Користувач не знайдений")
 public class UserNotFoundException extends RuntimeException {
 }
